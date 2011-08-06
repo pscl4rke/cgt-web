a8d75a42ab26cc75cffea119f02088d8d93d9883
P. S. Clarke <pscl4rke@googlemail.com>
2011-08-06 16:19:50 +0100

    Commenting the index file...
    To make the structure more obvious and remove the artifacts
    from the previous editing system.

--- cgt-theme/index.php
+++ cgt-theme/index.php
@@ -1,27 +1,20 @@
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/master.dwt" codeOutsideHTMLIsLocked="false" -->
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<!-- InstanceBeginEditable name="doctitle" -->
 <title>Covent Garden Talks</title>
-<!-- InstanceEndEditable -->
-
-
-<!--[if lt IE 7.]>
-<script defer type="text/javascript" src="scripts/pngfix.js"></script>
-<![endif]-->
-
-<!-- InstanceBeginEditable name="head" --><!-- InstanceEndEditable -->
 <link href="<?php bloginfo('template_url'); ?>/css/cgt-layout.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 <div id="container">
 	<div id="background">
+
  	<div id="page">
+
         <div id="logo"><img src="<?php bloginfo('template_url'); ?>/images/cgt-logo.png" alt="Fleet Street Talks" width="256" height="173" /></div>
-      <div id="mainmenu">
+
+        <div id="mainmenu">
                 <ul>
                   <li><a href="index.html">welcome</a></li>
                   <li><a href="lunchtime.html">lunchtime talk</a></li>
@@ -29,9 +22,9 @@
                   <li><a href="regulars.html">regulars</a></li>
                   <li><a href="contactus.html">contact/find us</a></li>
               </ul>
-      </div>
-		
-		<div id="content"><!-- InstanceBeginEditable name="Content" -->
+        </div><!-- end: mainmenu -->
+
+		<div id="content">
 		  <img src="images/welcome-title.png" alt="Welcome" width="164" height="45" />
 		  <p>Welcome to Covent Garden Talks!</p>
 		  <p>We are a network of Christians who work around the Covent Garden area. Our big aim is to help people working  in this part of London come face to face with the message of Jesus Christ.</p>
@@ -42,17 +35,22 @@
 	      </a><a href="christianityexplored.html">Christianity Explored</a><br />
 	      <a href="regulars.html">Prayer meeting</a></p>
         <p>&nbsp;</p>
-		<!-- InstanceEndEditable --></div>
-		
-		<div id="rightcolumn"><!-- InstanceBeginEditable name="RightColumn" -->
+		</div><!-- end: content -->
+
+		<div id="rightcolumn">
 		  <h1><img src="images/OctaveC.jpg" width="210" height="147" /></h1>
 		  <h1>&nbsp;</h1>
-		  <!-- InstanceEndEditable --></div>
- 	</div>
+        </div><!-- end: rightcolumn -->
+
+ 	</div><!-- end: page -->
+
 	<div id="footer">
-	<p>copyright &copy; <a href="http://www.coventgardentalks.org">covent garden talks  </a> : site by <a href="http://www.bendesmond.com" target="_blank">bendesmond.com</a></p>
-	</div></div>
-</div>
+        <p>copyright &copy; <a href="http://www.coventgardentalks.org">covent garden talks</a>
+            : site by <a href="http://www.bendesmond.com" target="_blank">bendesmond.com</a></p>
+    </div><!-- end: footer -->
+
+    </div><!-- end: background -->
+</div><!-- end: container -->
 </body>
-<!-- InstanceEnd --></html>
+</html>
 
