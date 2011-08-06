dc4f15fc1ba30f75a955f36b908d8fe6cd979552
P. S. Clarke <pscl4rke@googlemail.com>
2011-08-06 20:26:00 +0100

    Switching to content from database...
    Note that this still does not solve the problem of
    the sidebar.

--- cgt-theme/index.php
+++ cgt-theme/index.php
@@ -1,18 +1,12 @@
 
 <?php get_header(); ?>
 
-		<div id="content">
-		  <img src="images/welcome-title.png" alt="Welcome" width="164" height="45" />
-		  <p>Welcome to Covent Garden Talks!</p>
-		  <p>We are a network of Christians who work around the Covent Garden area. Our big aim is to help people working  in this part of London come face to face with the message of Jesus Christ.</p>
-	      <p>We welcome people at any stage of Christian understanding,  whether you would call yourself a Christian or are just interested in finding  out more about the Christian faith.</p>
-	      <p>&nbsp;</p>
-	      <p>What's on?</p>
-        <p><a href="lunchtime.html">Lunchtime Talk<br />
-	      </a><a href="christianityexplored.html">Christianity Explored</a><br />
-	      <a href="regulars.html">Prayer meeting</a></p>
-        <p>&nbsp;</p>
-		</div><!-- end: content -->
+        <div id="content">
+            <?php if (have_posts()) while (have_posts()) : the_post(); ?>
+                <h1><?php the_title() ?></h1>
+                <?php the_content() ?>
+            <?php endwhile; ?>
+        </div><!-- end: content -->
 
 		<div id="rightcolumn">
 		  <h1><img src="images/OctaveC.jpg" width="210" height="147" /></h1>
