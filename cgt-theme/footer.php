21f35c609ba3c1b5ea825c1d9daa4a2d573eb773
P. S. Clarke <pscl4rke@googlemail.com>
2011-08-06 18:41:34 +0100

    Splitting into header/footer files.

---
+++ cgt-theme/footer.php
@@ -0,0 +1,14 @@
+
+    </div><!-- end: page -->
+
+    <div id="footer">
+        <p>copyright &copy; <a href="http://www.coventgardentalks.org">covent garden talks</a>
+            : design by <a href="http://www.bendesmond.com" target="_blank">bendesmond.com</a>
+            : <a href="<?php echo admin_url() ?>" target="_blank">managed</a> by <a href="http://www.wordpress.org">wordpress</a></p>
+    </div><!-- end: footer -->
+
+    </div><!-- end: background -->
+</div><!-- end: container -->
+</body>
+</html>
+
